3c41a8cb91e92d2befc836329d691849520d694d
AndLego <galeon.gb@gmail.com>
2022-07-28 13:56:09 -0500

    try 1.0

--- src/context/MainContext.jsx
+++ src/context/MainContext.jsx
@@ -7,7 +7,7 @@ const MainProvider = (props) => {
   const [user, setUser] = React.useState("");
   const [room, setRoom] = React.useState("choose");
 
-  let socket = io("https://andlego-chat-app-api.herokuapp.com/");
+  let socket = io("https://git.heroku.com/andlego-chat-app.git");
   
   return (
     <MainContext.Provider
